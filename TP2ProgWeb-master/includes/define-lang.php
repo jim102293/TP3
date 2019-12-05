448509b6c8288f78c682afafd6b99c4596c14314
Seb <testw>
2019-12-05 11:22:26 -0500

    Update define-lang.php
    Nouveau define-lang avec connection à la base de données

--- TP2ProgWeb-master/includes/define-lang.php
+++ TP2ProgWeb-master/includes/define-lang.php
@@ -6,11 +6,58 @@ session_start();
 if(!isset ($_SESSION['lang'])){
     $_SESSION['lang']=1;
 };
+// Valeur default si la connexion à la base de données marche pas
+$home= 'Accueil';
+$subscribe = 'S\'inscrire';
+$localise = 'Localiser une activité';
+$natation = 'Natation';
+$badminton = 'Badminton';
+$randonnee = 'Randonnée';
+$kayak = 'Kayak';
+$velo = 'Vélo';
+$echec = 'Échecs';
+$student = 'Je suis un étudiant';
 
-
+// Choisir la langue dependant de la session
 if($_SESSION['lang'] == 1){
-    include('includes/lang-fr.php');
+    $language = 'fr';
 }else if($_SESSION['lang']== 2){
-    include('includes/lang-en.php');
-}
+    $language = 'en';    
+};
+
+// Fetch les données de la base des données
+$sql = "SELECT keyvalue, en, fr FROM `dictionary`";
+foreach($pdo->query($sql, PDO::FETCH_ASSOC) as $row){
+// Mettre l'entrée des base des données et les tags ensemble
+    if ($row['keyvalue'] == 'home'){
+    $home = $row[$language];
+    }     
+    else if ($row['keyvalue'] == 'subscribe'){
+        $subscribe = $row[$language];
+    }
+    else if ($row['keyvalue'] == 'localise'){
+        $localise = $row[$language];
+    }
+    else if ($row['keyvalue'] == 'natation'){
+        $natation = $row[$language];
+    }
+    else if ($row['keyvalue'] == 'badminton'){
+        $badminton = $row[$language];
+    }
+    else if ($row['keyvalue'] == 'randonnee'){
+        $randonnee = $row[$language];
+    }
+    else if ($row['keyvalue'] == 'kayak'){
+        $kayak = $row[$language];
+    }
+    else if ($row['keyvalue'] == 'velo'){
+        $velo = $row[$language];
+    }
+    else if ($row['keyvalue'] == 'echec'){
+        $echec = $row[$language];
+    }
+    else if ($row['keyvalue'] == 'student'){
+        $student = $row[$language];
+    }
+}      
 ?>
